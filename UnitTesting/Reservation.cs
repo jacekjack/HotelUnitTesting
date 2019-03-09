152530f6658ebce7a820de1e676df9d99ee10ddd
Jacek Tomasik <jacekjack123@gmail.com>
2019-03-09 08:43:52 +0100

    First Commit

---
+++ UnitTesting/Reservation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting
+{
+    public class Reservation
+    {
+        public User MadeBy { get; set; }
+
+        public bool CanBeCancel(User user)
+        {
+            if (user.Admin || MadeBy == user)
+                return true;
+
+            return false;
+        }
+
+        public bool RoomReservation(Room room, User user)
+        {
+            if (room.Vacant)
+            {
+                room.Roomate = user;
+                return true;
+            }
+            return false;
+
+        }
+         
+        public class User
+        {
+            public bool Admin { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class Room
+        {
+            public bool Vacant { get; set; }
+            public int Number { get; set; }
+            public User Roomate { get; set; }
+        }
+    }
+}
